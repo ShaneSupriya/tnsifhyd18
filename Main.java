a3ca261c66dcc89d6f1020dc58e0bc703b6cbe89
ShaneSupriya <168424456+ShaneSupriya@users.noreply.github.com>
2024-04-30 07:13:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.si.sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int c=90;
+		System.out.println(c);
+		Normal n1=new Normal();
+		System.out.println(n1.name);
+		n1.display();
+		System.out.println(Normal.place);
+		Normal.display1();
+
+	}
+
+}
